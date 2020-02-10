9a59ddaa7b48185943ffc04a2900aa8d78f90bf9
felipe1982 <felipe1982@users.noreply.github.com>
2020-02-11 01:44:41 +1000

    lambda: just upsert a route 53 resource record

--- lambda/acm-dns-validate.py
+++ lambda/acm-dns-validate.py
@@ -1,38 +1,47 @@
 #!/usr/bin/env python3
 from crhelper import CfnResource
+import logging
 import boto3
 
-NAME = 'boto3.felipe1982.com.'
-TYPE = 'CNAME'
-VALUE = '192.0.2.100'
-HOSTEDZONEID = 'Z1JYG3XEZZ05O1'
-route53_client = boto3.client("route53")
-def resource_record_exists(route53_client, HostedZoneId, StartRecordName=None, StartRecordType=None):
+logger = logging.getLogger(__name__)
+
+# NAME = 'boto3.felipe1982.com.'
+# TYPE = 'CNAME'
+# VALUE = '192.0.2.100'
+# HOSTEDZONEID = 'Z1JYG3XEZZ05O1'
+helper = CfnResource()
+try:
+    route53_client = boto3.client("route53")
+except Exception as e:
+    helper.init_failure(e)
+
+def resource_record_exists(event, context):
     try:
         response = route53_client.list_resource_record_sets(
-            HostedZoneId=HostedZoneId,
-            StartRecordName=StartRecordName,
-            StartRecordType=StartRecordType,
+            HostedZoneId=event['ResourceProperties']['HostedZoneId'],
+            StartRecordName=event['ResourceProperties']['Name'],
+            StartRecordType=event['ResourceProperties']['Type'],
             MaxItems='1'
         )
     except:
         raise
-    return response['ResourceRecordSets'][0]['Name'] == StartRecordName
-def upsert_record(route53_client, hostedzoneid, name, type, value):
+    return response['ResourceRecordSets'][0]['Name'] == event['ResourceProperties']['Name']
+# @helper.create
+def create(event, context):
     try:
         response = route53_client.change_resource_record_sets(
-            HostedZoneId=hostedzoneid,
+            HostedZoneId=event['ResourceProperties']['HostedZoneId'],
             ChangeBatch={
                 'Changes': [
                     {
                         'Action':'UPSERT',
                         'ResourceRecordSet': {
-                            'Name': name,
-                            'Type': type,
+                            'Name': event['ResourceProperties']['Name'],
+                            'Type': event['ResourceProperties']['Type'],
                             'TTL': 86400,
                             'ResourceRecords': [
                                 {
-                                    'Value': value
+                                    'Value': event['ResourceProperties']['Value']
                                 }
                             ]
                         }
@@ -43,22 +52,22 @@ def upsert_record(route53_client, hostedzoneid, name, type, value):
     except:
         raise
     return response
-
-def delete_record(route53_client, hostedzoneid, name, type, value):
+# @helper.delete
+def delete_record(event, context):
     try:
         response = route53_client.change_resource_record_sets(
-            HostedZoneId=hostedzoneid,
+            HostedZoneId=event['ResourceProperties']['HostedZoneId'],
             ChangeBatch={
                 'Changes': [
                     {
                         'Action':'DELETE',
                         'ResourceRecordSet': {
-                            'Name': name,
-                            'Type': type,
+                            'Name': event['ResourceProperties']['Name'],
+                            'Type': event['ResourceProperties']['Type'],
                             'TTL': 86400,
                             'ResourceRecords': [
                                 {
-                                    'Value': value
+                                    'Value': event['ResourceProperties']['Value']
                                 }
                             ]
                         }
@@ -69,22 +78,33 @@ def delete_record(route53_client, hostedzoneid, name, type, value):
     except:
         raise
     return response
-upsert_record(route53_client, HOSTEDZONEID, NAME, TYPE, VALUE)
 
-if resource_record_exists(route53_client, HOSTEDZONEID, NAME, TYPE):
-    print(f"Hooray! {NAME} exists")
-    delete_record(route53_client, HOSTEDZONEID, NAME, TYPE, VALUE)
-    if not resource_record_exists(route53_client, HOSTEDZONEID, NAME, TYPE):
-        print(f"{NAME} was deleted from HostedZoneId {HOSTEDZONEID}")
-else:
-    print(f"{NAME} does not exist")
-# # what do I need?
+def handler(event, context):
+    # helper(event, context)
+    create(event, context)
 
-# - hosted zone name (or hosted zone id)
-# - zone DNS name (example.com)
-# - Resource Record to add (xyz.example.com)
-# - CNAME to point to (xyz.acm-validation.aws)
-# - certificate ARN
-#   - name
-#   - value
-#   - type of domain for TLS certificate
+    if resource_record_exists(event, context):
+        print(f"Hooray! {NAME} exists")
+        delete_record(event, context)
+        if not resource_record_exists(event, context):
+            print(f"{NAME} was deleted from HostedZoneId {HOSTEDZONEID}")
+    else:
+        print(f"{NAME} does not exist")
+
+if __name__ == '__main__':
+    event = {
+        "RequestType" : "Create",
+        "ResponseURL" : "http://pre-signed-S3-url-for-response",
+        "StackId" : "arn:aws:cloudformation:us-west-2:123456789012:stack/stack-name/guid",
+        "RequestId" : "unique id for this create request",
+        "ResourceType" : "Custom::TestResource",
+        "LogicalResourceId" : "MyTestResource",
+        "ResourceProperties" : {
+            "HostedZoneId" : "Z1JYG3XEZZ05O1",
+            "Name" : "boto3.felipe1982.com",
+            "Type" : "CNAME",
+            "Value" : "aws.amazon.com"
+        }
+    }
+    context = None
+    handler(event, context)

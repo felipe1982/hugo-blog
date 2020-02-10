205dd05f4015d4e7c4230e951a58f04adf571b62
felipe1982 <felipe1982@users.noreply.github.com>
2020-02-10 21:05:45 +1000

    lambda: check if record exists

--- lambda/acm-dns-validate.py
+++ lambda/acm-dns-validate.py
@@ -1,73 +1,83 @@
 #!/usr/bin/env python3
 from crhelper import CfnResource
 import boto3
-import re
-import time
 
 NAME = 'boto3.felipe1982.com.'
-TYPE = 'A'
+TYPE = 'CNAME'
 VALUE = '192.0.2.100'
 HOSTEDZONEID = 'Z1JYG3XEZZ05O1'
 route53_client = boto3.client("route53")
+def resource_record_exists(route53_client, HostedZoneId, StartRecordName=None, StartRecordType=None):
+    try:
+        response = route53_client.list_resource_record_sets(
+            HostedZoneId=HostedZoneId,
+            StartRecordName=StartRecordName,
+            StartRecordType=StartRecordType,
+            MaxItems='1'
+        )
+    except:
+        raise
+    return response['ResourceRecordSets'][0]['Name'] == StartRecordName
 def upsert_record(route53_client, hostedzoneid, name, type, value):
-    route53_client.change_resource_record_sets(
-        HostedZoneId=hostedzoneid,
-        ChangeBatch={
-            'Changes': [
-                {
-                    'Action':'UPSERT',
-                    'ResourceRecordSet': {
-                        'Name': name,
-                        'Type': type,
-                        'TTL': 300,
-                        'ResourceRecords': [
-                            {
-                                'Value': value
-                            }
-                        ]
+    try:
+        response = route53_client.change_resource_record_sets(
+            HostedZoneId=hostedzoneid,
+            ChangeBatch={
+                'Changes': [
+                    {
+                        'Action':'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': name,
+                            'Type': type,
+                            'TTL': 86400,
+                            'ResourceRecords': [
+                                {
+                                    'Value': value
+                                }
+                            ]
+                        }
                     }
-                }
-            ]
-        }
-    )
+                ]
+            }
+        )
+    except:
+        raise
+    return response
+
 def delete_record(route53_client, hostedzoneid, name, type, value):
-    route53_client.change_resource_record_sets(
-        HostedZoneId=hostedzoneid,
-        ChangeBatch={
-            'Changes': [
-                {
-                    'Action':'DELETE',
-                    'ResourceRecordSet': {
-                        'Name': name,
-                        'Type': type,
-                        'TTL': 300,
-                        'ResourceRecords': [
-                            {
-                                'Value': value
-                            }
-                        ]
+    try:
+        response = route53_client.change_resource_record_sets(
+            HostedZoneId=hostedzoneid,
+            ChangeBatch={
+                'Changes': [
+                    {
+                        'Action':'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': name,
+                            'Type': type,
+                            'TTL': 86400,
+                            'ResourceRecords': [
+                                {
+                                    'Value': value
+                                }
+                            ]
+                        }
                     }
-                }
-            ]
-        }
-    )
-delete_record(route53_client, HOSTEDZONEID, NAME, TYPE, VALUE)
+                ]
+            }
+        )
+    except:
+        raise
+    return response
 upsert_record(route53_client, HOSTEDZONEID, NAME, TYPE, VALUE)
 
-def get_zone_id_from_name(zone_name):
-    pass
-def upsert_dns_resource_record(route53_client, zone_id, name, value):
-    pass
-def get_cname_from_cert():
-    pass
-
-def handler(event, context):
-    print("printing event")
-    print(event)
-    print("printing contenxt")
-    print(context)
-
-
+if resource_record_exists(route53_client, HOSTEDZONEID, NAME, TYPE):
+    print(f"Hooray! {NAME} exists")
+    delete_record(route53_client, HOSTEDZONEID, NAME, TYPE, VALUE)
+    if not resource_record_exists(route53_client, HOSTEDZONEID, NAME, TYPE):
+        print(f"{NAME} was deleted from HostedZoneId {HOSTEDZONEID}")
+else:
+    print(f"{NAME} does not exist")
 # # what do I need?
 
 # - hosted zone name (or hosted zone id)

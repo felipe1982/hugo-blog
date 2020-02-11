24bdb11850b9905d3e73c68b94c01c39fcd4da75
felipe1982 <felipe1982@users.noreply.github.com>
2020-02-11 20:34:52 +1000

    remove try/except from functions

--- lambda/certificate_provider.py
+++ lambda/certificate_provider.py
@@ -12,15 +12,12 @@ def request_certificate(DomainName, SubjectAlternativeNames):
     )
     return response['CertificateArn']
 
-def get_certificate_cname_value(arn):
-    try:
-        response = acm_client.describe_certificate(
-            CertificateArn=arn
-        )
-        cname_list = [ (resource_record['ResourceRecord']['Name'], resource_record['ResourceRecord']['Type'], resource_record['ResourceRecord']['Value']) for resource_record in response['Certificate']['DomainValidationOptions'] ]
+def get_certificate_cname_value(CertificateArn):
+    response = acm_client.describe_certificate(
+        CertificateArn=CertificateArn
+    )
+    cname_list = [ (resource_record['ResourceRecord']['Name'], resource_record['ResourceRecord']['Type'], resource_record['ResourceRecord']['Value']) for resource_record in response['Certificate']['DomainValidationOptions'] ]
 
-    except:
-        raise
     return cname_list
-# request_certificate('boto3.felipe1982.com', ['www.felipe1982.com'])
+
 pprint(get_certificate_cname_value('arn:aws:acm:us-east-1:638088845137:certificate/def5f55f-13b6-49dd-9d85-477d19f275ef'))

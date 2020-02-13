668f771ce19928368e3afd6957c522362efa6614
felipe1982 <felipe1982@users.noreply.github.com>
2020-02-14 00:05:40 +1000

    crhelper: add crhelper to make Custom Resource

--- lambda/certificate_provider.py
+++ lambda/certificate_provider.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 import boto3
+from crhelper import CfnResource
 acm_client = boto3.client('acm')
-
-def request_certificate(DomainName, SubjectAlternativeNames):
-    response = acm_client.request_certificate(
-        DomainName=DomainName,
-        SubjectAlternativeNames=SubjectAlternativeNames,
-        ValidationMethod='DNS'
-    )
-    return response['CertificateArn']
+helper = CfnResource()
+@helper.create
+def create(event, context):
+    DomainName = event['ResourceProperties']['DomainName']
+    SubjectAlternativeNames = event['ResourceProperties'].get('SubjectAlternativeNames', None)
+    CertificateArn = request_certificate(DomainName, SubjectAlternativeNames)
+    helper.Data.update(CertificateArn)
+def request_certificate(DomainName, SubjectAlternativeNames=None):
+    if SubjectAlternativeNames:
+        response = acm_client.request_certificate(
+            DomainName=DomainName,
+            SubjectAlternativeNames=SubjectAlternativeNames,
+            ValidationMethod='DNS'
+        )
+    else:
+        response = acm_client.request_certificate(
+            DomainName=DomainName,
+            ValidationMethod='DNS'
+        )
+    return {"CertificateArn" : response['CertificateArn']}
+def handler(event, context):
+    helper(event, context)
